947155b9a2ebb18b6d1273e1bc3cf6a3354e2101
久米 史也 <fumiya.kume@hotmail.com>
2017-01-27 22:22:58 +0900

    Add Database DataModel

---
+++ LTTimer/LTTimer.Azure/DataModel/TimerTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LTTimer.Azure.DataModel
+{
+    public class TimerTable
+    {
+        public string id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+    }
+}
