b7cd98be55397a4f66b122268c01753f3ddebc3e
久米 史也 <fumiya.kume@hotmail.com>
2017-02-08 22:05:20 +0900

    Publish UWP Application

---
+++ LTTimer/LTTimer/LTTimer/Model/CountdownTimer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reactive.Linq;
+using Reactive.Bindings;
+using Reactive.Bindings.Notifiers;
+
+namespace LTTimer.Model
+{
+    public class CountdownTimer
+    {
+        public ReactiveTimer Timer { get; set; } = new ReactiveTimer(TimeSpan.FromSeconds(1.0));
+        public CountNotifier Secounds { get; set; } = new CountNotifier(60 * 5);
+
+        public CountdownTimer()
+        {
+            Timer.Subscribe(l =>
+            {
+                if (Secounds.Count <= 1) Timer.Stop();
+                Secounds.Decrement();
+            });
+        }
+
+        public void Start(int limit)
+        {
+            if (limit <= 0) throw new ArgumentOutOfRangeException(nameof(limit));
+            Secounds.Increment(limit);
+            Timer.Start();
+        }
+    }
+}
