efb43754bf3b44e8ac0551e96a16af91379d8024
久米史也 <fumiya.kume@hotmail.com>
2017-03-21 20:17:39 +0900

    設定の値が変更時に自動保存するようにした

--- LTTimer/LTTimer/LTTimer/ViewModels/SettingPageViewModel.cs
+++ LTTimer/LTTimer/LTTimer/ViewModels/SettingPageViewModel.cs
@@ -3,16 +3,18 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using LTTimer.Model;
 using Reactive.Bindings;
 
 namespace LTTimer.ViewModels
 {
     public class SettingPageViewModel : BindableBase
     {
-        public ReactiveProperty<bool> IsSoundWhenTimerFisnish { get; set; } = new ReactiveProperty<bool>(true);
+        public ReactiveProperty<bool> IsSoundWhenTimerFisnish { get; set; } = new ReactiveProperty<bool>(SoundEffectSettingService.GeneralSettings);
         public SettingPageViewModel()
         {
-
+            IsSoundWhenTimerFisnish
+                .Subscribe(b => SoundEffectSettingService.GeneralSettings = b);
         }
     }
 }
