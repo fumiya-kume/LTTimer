f0afac7f18e9067d85932da4b9f432abe1445633
Fumiya Kume <fumiya.kume@hotmail.com>
2017-03-27 20:27:33 +0900

    Install Xamarin MediaManager Plugin

--- LTTimer/LTTimer.UITest/AppInitializer.cs
+++ LTTimer/LTTimer.UITest/AppInitializer.cs
@@ -9,7 +9,7 @@ namespace LTTimer.UITest
     public class AppInitializer
     {
         public static string APKpath { get; set; } =
-            @"C:\Users\gurag\AppData\Local\Xamarin\Mono for Android\Archives\2017-03-27\LTTimer.Droid 3-27-17 8.21 AM.apkarchive\LTTimer.Droid.apk";
+            @"C:\Users\gurag\AppData\Local\Xamarin\Mono for Android\Archives\2017-03-27\LTTimer.Droid 3-27-17 8.00 PM.apkarchive\LTTimer.Droid.apk";
 
 
         public static IApp StartApp(Platform platform)
